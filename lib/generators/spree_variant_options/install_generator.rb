77e0234879890184c00b1402e0326f85f3807626
Pedro Pinto <pedropinto.inbox@gmail.com>
2015-02-13 11:47:01 +0000

    Fixed javascript and stylesheet includes

--- lib/generators/spree_variant_options/install_generator.rb
+++ lib/generators/spree_variant_options/install_generator.rb
@@ -9,13 +9,13 @@ module SpreeVariantOptions
       end
 
       def add_javascripts
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/variant_options\n"
-        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/product_variant_options\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_variant_options\n"
+        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require spree/backend/variant_images\n"
       end
 
       def add_stylesheets
-        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", "*= require spree/frontend/variant_options\n", :before => /\*\//, :verbose => true
-        inject_into_file "vendor/assets/stylesheets/spree/backend/all.css", "*= require spree/backend/variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", "*= require spree/frontend/spree_variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/backend/all.css", "*= require spree/backend/spree_variant_options\n", :before => /\*\//, :verbose => true
       end
 
     end
