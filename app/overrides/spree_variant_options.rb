4b8d2b517cf195f05134c32b246c2a27c083dc3c
redglory <pedropinto.inbox@gmail.com>
2015-03-02 18:53:40 +0100

    disable cart_form

--- app/overrides/spree_variant_options.rb
+++ app/overrides/spree_variant_options.rb
@@ -1,8 +1,8 @@
-Deface::Override.new(:virtual_path   => "spree/products/_cart_form",
-                     :name           => "spree_variant_options",
-                     :replace        => "#product-variants",
-                     :partial        => "spree/products/variant_options",
-                     :disabled       => false)
+#Deface::Override.new(:virtual_path   => "spree/products/_cart_form",
+#                     :name           => "spree_variant_options",
+#                     :replace        => "#product-variants",
+#                     :partial        => "spree/products/variant_options",
+#                     :disabled       => false)
 
 Deface::Override.new(:virtual_path   => "spree/admin/option_types/edit",
                      :name           => "admin_option_value_table_headers",
