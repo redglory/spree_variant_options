1095ca2b7d763e7af86c5972f0c44f68d2de2b0a
redglory <pedropinto.inbox@gmail.com>
2015-07-17 13:54:29 -0400

    trying line item adjustment

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,13 +3,10 @@ Spree::Variant.class_eval do
   include ActionView::Helpers::NumberHelper
 
   def to_hash
-    #actual_price  = self.price
-    #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
     {
       :id    => self.id,
       :in_stock => self.in_stock?,
       :price => self.display_price
-      #:price => number_to_currency(actual_price)
     }
   end
 
